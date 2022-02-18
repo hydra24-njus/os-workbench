d12e21d7fbb85720eafa0deacc6bd434d45c12f8
tracer-nju <tracer@nju.edu.cn>
2022-02-18 15:52:05 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:52:05 up 11 min,  1 user,  load average: 0.09, 0.14, 0.09

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <assert.h>
 
+const char *path ="/proc";
+
+
+typedef struct {
+  bool show_pids;
+  bool version;
+  bool numeric_sort;
+}Options;
+Options state;
+
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
+    if(strcmp(argv[i],"-V")==0)state.version=1;
+    else if(strcmp(argv[i],"-p")==0)state.show_pids=1;
+    else if(strcmp(argv[i],"-n")==0)state.numeric_sort=1;
+    else {
+      printf("error cle option.\n");
+      assert(0);
+    }
   }
   assert(!argv[argc]);
   return 0;
