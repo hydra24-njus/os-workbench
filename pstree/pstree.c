558b44741c56716e03cc1686961eb73cd3b7255b
tracer-nju <tracer@nju.edu.cn>
2022-02-18 18:33:04 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:33:04 up 42 min,  1 user,  load average: 0.17, 0.14, 0.15

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -17,18 +17,18 @@ int main(int argc, char *argv[]) {
   for (int i = 1; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
-    /*if(strcmp(argv[i],"-V")==0)state.version=1;
+    if(strcmp(argv[i],"-V")==0)state.version=1;
     else if(strcmp(argv[i],"-p")==0)state.show_pids=1;
     else if(strcmp(argv[i],"-n")==0)state.numeric_sort=1;
     else {
       printf("error cle option.\n");
       assert(0);
-    }*/
+    }
   }
   assert(!argv[argc]);
-  /*if(state.version){
-    printf("pstree 1.0\n\tCopyright (C) 2022 hydra24.\n");
+  if(state.version){
+    perror("pstree 1.0\n\tCopyright (C) 2022 hydra24.\n");
     return 0;
-  }*/
+    
   return 0;
 }
