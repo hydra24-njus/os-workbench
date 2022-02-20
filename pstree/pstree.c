9cf7f8a31635acaf10043eda8d5c410b36a74067
tracer-nju <tracer@nju.edu.cn>
2022-02-21 00:14:47 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:14:47 up  3:05,  1 user,  load average: 0.35, 0.26, 0.25

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -59,8 +59,8 @@ int getprocess(int n){
     strncpy(process[cnt].name,buf+k+1,k1-k-2);
     //printf("%s\n",buf+k1+1);
     sscanf(buf+k1+1,"%c %d",&process[cnt].state,&process[cnt].ppid);
-    printf("%d (%s) %c %d\n",process[cnt].pid,process[cnt].name,process[cnt].state,process[cnt].ppid);
-    printf("%s\n\n",buf);
+    //printf("%d (%s) %c %d\n",process[cnt].pid,process[cnt].name,process[cnt].state,process[cnt].ppid);
+    //printf("%s\n\n",buf);
     cnt++;
   }
   return cnt;
@@ -87,6 +87,6 @@ int main(int argc, char *argv[]) {
   }
   int n=getprocessfolder();
   n=getprocess(n);
-  //for(int i=0;i<n;i++)printf("%d\t%s\n",process[i].pid,process[i].name);
+  for(int i=0;i<n;i++)printf("%d\t%s\n",process[i].pid,process[i].name);
   return 0;
 }
