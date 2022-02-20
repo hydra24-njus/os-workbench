eb0b263fe4dba2937922d44b23020a00b538d718
tracer-nju <tracer@nju.edu.cn>
2022-02-20 23:08:56 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:08:56 up  1:59,  1 user,  load average: 0.50, 0.59, 0.48

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -3,7 +3,7 @@
 // Operating system is a C program!
 int main(const char *args) {
   ioe_init();
-
+printf("111\n");
   puts("mainargs = \"");
   puts(args); // make run mainargs=xxx
   puts("\"\n");
