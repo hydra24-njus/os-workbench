2b991d4a2d0ba9f3f3c55b29ccad66276340ca70
tracer-nju <tracer@nju.edu.cn>
2022-02-21 00:42:29 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:42:29 up  3:33,  1 user,  load average: 0.13, 0.22, 0.23

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -56,7 +56,7 @@ int getprocess(int n){
     while(buf[k++]!=' ');
     int k1=64;
     while(buf[k1--]!=')');
-    k1++;
+    k1+=2;
     strncpy(process[cnt].name,buf+k+1,k1-k-2);
     //printf("%s\n",buf+k1+1);
     sscanf(buf+k1+1,"%c %d",&process[cnt].state,&process[cnt].ppid);
