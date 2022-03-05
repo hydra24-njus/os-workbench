0a06a0f557cea466f7410ec49086673c660705f2
tracer-nju <tracer@nju.edu.cn>
2022-03-05 16:03:02 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:03:02 up 5 min,  1 user,  load average: 0.19, 0.31, 0.18

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,11 +1,5 @@
 #include <game.h>
 
-#define KEYNAME(key) \
-  [AM_KEY_##key] = #key,
-static const char *key_names[] = {
-  AM_KEYS(KEYNAME)
-};
-
 // Operating system is a C program!
 int main(const char *args) {
   ioe_init();
@@ -17,14 +11,8 @@ int main(const char *args) {
 
   puts("Press any key to see its key code...\n");
   while (1) {
-    AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
-    ioe_read(AM_INPUT_KEYBRD, &event);
-    if (event.keycode != AM_KEY_NONE && event.keydown) {
-      puts("Key pressed: ");
-      puts(key_names[event.keycode]);
-      puts("\n");
-      if(strcmp(key_names[event.keycode],"ESCAPE")==0)halt(0);
-    }
+    char ch='a';print_key();
+    printf("%s\n",ch);
   }
   return 0;
 }
--- amgame/src/keyboard.c
+++ amgame/src/keyboard.c
@@ -15,3 +15,19 @@ void print_key() {
     if(strcmp(key_names[event.keycode],"ESCAPE")==0)halt(0);
   }
 }
+
+char read_key(){
+  AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
+  ioe_read(AM_INPUT_KEYBRD, &event);
+  if (event.keycode != AM_KEY_NONE && event.keydown) {
+    puts("Key pressed: ");
+    puts(key_names[event.keycode]);
+    puts("\n");
+    if(strcmp(key_names[event.keycode],"ESCAPE")==0)halt(0);
+    else if(strcmp(key_names[event.keycode],"A")==0)return 'a';
+    else if(strcmp(key_names[event.keycode],"W")==0)return 'w';
+    else if(strcmp(key_names[event.keycode],"D")==0)return 'd';
+    else if(strcmp(key_names[event.keycode],"S")==0)return 's';
+  }
+  return 0;
+}
