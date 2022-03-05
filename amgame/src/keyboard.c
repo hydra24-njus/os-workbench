d516ac71886ecd807eaa3e97b598bb6669b9a3bc
tracer-nju <tracer@nju.edu.cn>
2022-03-05 16:17:29 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:17:29 up 20 min,  1 user,  load average: 0.21, 0.28, 0.24

--- amgame/src/keyboard.c
+++ amgame/src/keyboard.c
@@ -20,9 +20,6 @@ char read_key(){
   AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
   ioe_read(AM_INPUT_KEYBRD, &event);
   if (event.keycode != AM_KEY_NONE && event.keydown) {
-    puts("Key pressed: ");
-    puts(key_names[event.keycode]);
-    puts("\n");
     if(strcmp(key_names[event.keycode],"ESCAPE")==0)halt(0);
     else if(strcmp(key_names[event.keycode],"A")==0)return 'a';
     else if(strcmp(key_names[event.keycode],"W")==0)return 'w';
