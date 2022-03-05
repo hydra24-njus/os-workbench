fb3d1742d38f38c41e44f673f0984dba7e3c9915
tracer-nju <tracer@nju.edu.cn>
2022-03-05 16:20:54 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:20:54 up 23 min,  1 user,  load average: 0.34, 0.38, 0.29

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -13,10 +13,10 @@ int a=w/2,b=h/2;
   puts("Press any key to see its key code...\n");
   while (1) {
     char ch=read_key();
-    if(ch=='a')a=(a-1)>0?a-1:a-1+16;
-    if(ch=='d')a=(a+1)<16?a+1:a+1-16;
-    if(ch=='w')b=(b-1)>0?b-1:b-1+16;
-    if(ch=='s')b=(b+1)<16?b+1:b+1-16;
+    if(ch=='a')a=(a-1)>0?a-1:a-1+w;
+    if(ch=='d')a=(a+1)<16?a+1:a+1-w;
+    if(ch=='w')b=(b-1)>0?b-1:b-1+h;
+    if(ch=='s')b=(b+1)<16?b+1:b+1-h;
     draw_action(a,b);
   }
   return 0;
