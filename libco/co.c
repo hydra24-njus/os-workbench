4e811c26bb61ea8d50cd9917855291f49d83b932
tracer-nju <tracer@nju.edu.cn>
2022-03-09 20:55:03 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:55:03 up 28 min,  1 user,  load average: 0.55, 0.33, 0.18

--- libco/co.c
+++ libco/co.c
@@ -1,15 +1,23 @@
 #include "co.h"
 #include <stdlib.h>
+#ifdef LOCAL_MACHINE
+  #define debug(...) printf(__VA_ARGS__)
+#else
+  #define debug()
+#endif
 
 struct co {
 };
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
+  debug("co_start");
   return NULL;
 }
 
 void co_wait(struct co *co) {
+  debug("co_wait");
 }
 
 void co_yield() {
+  debug("co_yield");
 }
