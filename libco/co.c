64bb3daee0541fb6384ece579821037c40f09a9e
tracer-nju <tracer@nju.edu.cn>
2022-03-11 17:45:25 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:45:25 up 32 min,  1 user,  load average: 0.15, 0.22, 0.32

--- libco/co.c
+++ libco/co.c
@@ -1,6 +1,7 @@
 #include "co.h"
 #include <stdlib.h>
 #include <stdint.h>
+#include <setjmp.h>
 #ifdef LOCAL_MACHINE
   #define debug(...) printf(__VA_ARGS__)
   #include<stdio.h>
@@ -8,7 +9,25 @@
   #define debug(...)
 #endif
 
+#define STACK_SIZE 1<<10
+
+enum co_status {
+  CO_NEW = 1, // 新创建，还未执行过
+  CO_RUNNING, // 已经执行过
+  CO_WAITING, // 在 co_wait 上等待
+  CO_DEAD,    // 已经结束，但还未释放资源
+};
+
+
 struct co {
+  char *name;
+  void (*func)(void *); // co_start 指定的入口地址和参数
+  void *arg;
+
+  enum co_status status;  // 协程的状态
+  struct co *    waiter;  // 是否有其他协程在等待当前协程
+  jmp_buf        context; // 寄存器现场 (setjmp.h)
+  uint8_t        stack[STACK_SIZE]; // 协程的堆栈
 };
 
 static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
