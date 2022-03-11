e9772d4c047956a48e14e3c9e30f70a8b0780e24
tracer-nju <tracer@nju.edu.cn>
2022-03-11 17:43:42 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:43:42 up 30 min,  1 user,  load average: 0.27, 0.28, 0.35

--- libco/co.c
+++ libco/co.c
@@ -1,5 +1,6 @@
 #include "co.h"
 #include <stdlib.h>
+#include <stdint.h>
 #ifdef LOCAL_MACHINE
   #define debug(...) printf(__VA_ARGS__)
   #include<stdio.h>
@@ -9,6 +10,27 @@
 
 struct co {
 };
+
+static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
+  asm volatile (
+#if __x86_64__
+    "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+      : : "b"((uintptr_t)sp), "d"(entry), "a"(arg) : "memory"
+#else
+    "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
+      : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg) : "memory"
+#endif
+  );
+}
+
+
+
+
+
+
+
+
+
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   debug("co_start\n");
