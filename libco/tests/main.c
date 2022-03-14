50e15a4f3b396a6809fced4202111a90945a0b6a
tracer-nju <tracer@nju.edu.cn>
2022-03-14 14:23:46 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:23:46 up  4:30,  1 user,  load average: 0.15, 0.29, 0.30

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -100,16 +100,16 @@ static void test_2() {
 
     struct co *thd1 = co_start("producer-1", producer, queue);
     struct co *thd2 = co_start("producer-2", producer, queue);
-    //struct co *thd3 = co_start("consumer-1", consumer, queue);
-    //struct co *thd4 = co_start("consumer-2", consumer, queue);
+    struct co *thd3 = co_start("consumer-1", consumer, queue);
+    struct co *thd4 = co_start("consumer-2", consumer, queue);
 printf("12\n");
     co_wait(thd1);
     co_wait(thd2);
 printf("34\n");
     g_running = 0;
 
-    //co_wait(thd3);
-    //co_wait(thd4);
+    co_wait(thd3);
+    co_wait(thd4);
 
     while (!q_is_empty(queue)) {
         do_consume(queue);
