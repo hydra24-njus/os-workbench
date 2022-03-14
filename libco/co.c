4bdc3a3f1b31b1fdb00d6c5160a858f71df5cfd2
tracer-nju <tracer@nju.edu.cn>
2022-03-14 11:01:44 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:01:44 up  1:08,  1 user,  load average: 0.36, 0.57, 0.49

--- libco/co.c
+++ libco/co.c
@@ -53,7 +53,6 @@ void co_wrapper(){
   current->status=CO_RUNNING;
   current->func(current->arg);
   current->status=CO_DEAD;
-  if(current->waiter)current->waiter->status=CO_RUNNING;
 }
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   for(int i=1;i<CO_MAX;i++){
@@ -73,7 +72,7 @@ void co_wait(struct co *co) {
   current->status=CO_WAITING;
   co->waiter=current;
   while(co->status!=CO_DEAD)co_yield();
-  co->waiter->status=CO_RUNNING;
+  if(current->waiter)current->waiter->status=CO_RUNNING;
 }
 
 void co_yield() {
