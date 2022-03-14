e3b98f5ada9354848715f6f9faabd462d338341b
tracer-nju <tracer@nju.edu.cn>
2022-03-14 13:26:18 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:26:18 up  3:32,  1 user,  load average: 0.09, 0.17, 0.13

--- libco/co.c
+++ libco/co.c
@@ -98,7 +98,7 @@ void co_yield() {
   int val=setjmp(current->context);
   if(val==0){
     for(int i=0;i<CO_MAX;i++){
-      if(coset[i].flag==true&&(((struct co*)coset[i].this)->status==CO_NEW||((struct co*)coset[i].this)->status==CO_RUNNING)){
+      if((void*)coset[i].this&&(((struct co*)coset[i].this)->status==CO_NEW||((struct co*)coset[i].this)->status==CO_RUNNING)){
         current=(struct co*)coset[i].this;
         break;
       }
