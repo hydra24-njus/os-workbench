6788aadf276558f9c5a31ec068dc6069e7d27cd3
tracer-nju <tracer@nju.edu.cn>
2022-03-20 02:02:38 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:02:38 up  1:01,  1 user,  load average: 0.21, 0.26, 0.45

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,7 +1,7 @@
 #include <common.h>
 #include <klib.h>
 void easy_test(){
-    uintptr_t ans=(uintptr_t)pmm->alloc(63);
+    uintptr_t ans=(uintptr_t)pmm->alloc(4000);
     printf("%d\n",ans);
 }
 int main() {
