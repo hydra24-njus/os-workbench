681dd3f2e4d1fba2f7ca81f381ff35057a9140bf
tracer-nju <tracer@nju.edu.cn>
2022-03-22 12:35:58 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:35:58 up 26 min,  1 user,  load average: 0.70, 0.51, 0.45

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,11 +1,7 @@
 #include <common.h>
 #include <klib.h>
-void easy_test(){
-    uintptr_t ans=(uintptr_t)pmm->alloc(2000);
-    printf("%d\n",ans);
-}
 int main() {
   os->init();
-  mpe_init(easy_test);
+  mpe_init(os->run);
   return 1;
 }
