7bd29e717df6f366c74b109a47c953f5ac2b520a
tracer-nju <tracer@nju.edu.cn>
2022-03-23 16:20:59 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:20:59 up 8 min,  1 user,  load average: 0.27, 0.35, 0.21

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -8,6 +8,7 @@ static void os_run() {
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     putch(*s == '*' ? '0' + cpu_current() : *s);
   }
+  for(int i=1;i<100;i++)pmm->alloc(i);
   while (1) ;
 }
 
