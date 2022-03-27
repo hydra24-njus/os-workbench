dcf5a6dfab1d05bc8f21070c1363d10710c0b4f7
tracer-nju <tracer@nju.edu.cn>
2022-03-28 00:01:50 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:01:50 up 4 min,  1 user,  load average: 0.17, 0.26, 0.12

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -8,6 +8,7 @@ static void *kalloc(size_t size) {
   while((1<<i)<size)i++;
   if(t%(1<<i)!=0)t=t+((1<<i)-t%(1<<i));
   tmp=(void*)(t+size);
+  if(tmp>heap.end)t=0;
   unlock(&biglock);
   return (void*)t;
 }
