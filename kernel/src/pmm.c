1ec25d59b3e8de5d73fdbdc1d722a1032874619e
tracer-nju <tracer@nju.edu.cn>
2022-03-28 00:01:07 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:01:07 up 3 min,  1 user,  load average: 0.28, 0.28, 0.12

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,16 +1,14 @@
 #include <common.h>
-void* tmp[7];
-void* tmp0[7];
+void* tmp;
 spinlock_t biglock;
 static void *kalloc(size_t size) {
-  
-  uintptr_t t=(uintptr_t)tmp[cpu_current()];
+  lock(&biglock);
+  uintptr_t t=(uintptr_t)tmp;
   int i=0;
   while((1<<i)<size)i++;
   if(t%(1<<i)!=0)t=t+((1<<i)-t%(1<<i));
-  tmp[cpu_current()]=(void*)t+size;
-  if(tmp[cpu_current()]>tmp[cpu_current()+1])return 0;
-  //assert(tmp<heap.end);
+  tmp=(void*)(t+size);
+  unlock(&biglock);
   return (void*)t;
 }
 
@@ -20,12 +18,7 @@ static void kfree(void *ptr) {
 static void pmm_init() {
   spinlock_init(&biglock);
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
-  uintptr_t ttt=pmsize/6;
-  for(int i=0;i<5;i++){
-    tmp[i]=(void*)((uintptr_t)heap.start+ttt*i);
-    tmp0[i]=tmp[i];
-  }
-  tmp0[6]=heap.end;
+  tmp=heap.start;
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 }
 
