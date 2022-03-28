c98434a5885a655139a412f5bc364a1693ce71d5
tracer-nju <tracer@nju.edu.cn>
2022-03-28 23:38:44 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:38:44 up  1:04,  1 user,  load average: 0.10, 0.24, 0.19

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -107,6 +107,7 @@ static void *kalloc(size_t size1) {
     unlock(&biglock);
             debug("newpage2\n");
     ptr->prev=tmp;
+            debug("newpage3\n");
     ptr->next=NULL;
     ptr->now=0;ptr->max=7168/size;ptr->type=size;
 
