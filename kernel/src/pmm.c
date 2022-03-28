569ffca793f33a9a736b4c4deadbe57728e0a1f0
201240105-Zhai Jingbo <201240105@smail.nju.edu.cn>
2022-03-28 19:38:00 +0800

    before github

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,25 +1,40 @@
 #include <common.h>
-void* tmp;
+uintptr_t heaptr;
 spinlock_t biglock;
+
+//辅助函数
+void _init(){
+  spinlock_init(&biglock);
+  heaptr=(uintptr_t)heap.start;
+}
+void* sbrk(int size){
+  uintptr_t tmp=heaptr;
+  heaptr+=size;
+  if(heaptr>heap.end)return NULL;
+  else return (void*)tmp;
+}
+unsigned int power2(unsigned int size){
+  size--;
+  size|=size>>1;
+  size|=size>>2;
+  size|=size>>4;
+  size|=size>>8;
+  size|=size>>16;
+  size++;
+  return size;
+}
+
 static void *kalloc(size_t size) {
-  lock(&biglock);
-  uintptr_t t=(uintptr_t)tmp;
-  int i=0;
-  while((1<<i)<size)i++;
-  if(t%(1<<i)!=0)t=t+((1<<i)-t%(1<<i));
-  tmp=(void*)(t+size);
-  if(tmp>heap.end)assert(0);
-  unlock(&biglock);
-  return (void*)t;
+
 }
 
 static void kfree(void *ptr) {
+
 }
 
 static void pmm_init() {
-  spinlock_init(&biglock);
+  _init();
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
-  tmp=heap.start;
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 }
 
