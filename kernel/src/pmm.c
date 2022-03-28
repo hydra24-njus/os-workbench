bf7b02db7ead51f50f3edf0ec69d7a7226f9a421
tracer-nju <tracer@nju.edu.cn>
2022-03-28 22:27:39 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:27:39 up  2:06,  1 user,  load average: 0.87, 0.73, 0.57

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -61,7 +61,67 @@ void* slowpath_alloc(size_t size){
 }
 
 static void *kalloc(size_t size) {
-  return NULL;
+  uintptr_t addr=0;
+  size=power2(size);
+  if(size>4096){
+    return slowpath_alloc(size);
+  }
+  struct page_t* ptr=NULL;
+  switch(size){
+    case 32  :ptr=buddy[cpu_current()].p32  ;break;
+    case 64  :ptr=buddy[cpu_current()].p64  ;break;
+    case 128 :ptr=buddy[cpu_current()].p128 ;break;
+    case 256 :ptr=buddy[cpu_current()].p256 ;break;
+    case 512 :ptr=buddy[cpu_current()].p512 ;break;
+    case 1024:ptr=buddy[cpu_current()].p1024;break;
+    case 2048:ptr=buddy[cpu_current()].p2048;break;
+    case 4096:ptr=buddy[cpu_current()].p4096;break;
+  }
+  if (ptr == NULL){ //该cpu没有页
+    lock(&biglock);
+    ptr = new_page();
+    ptr->prev=NULL;ptr->next=NULL;
+    ptr->now=0;ptr->max=7168/size;
+    unlock(&biglock);
+    switch (size){
+    case 32  :ptr->type=32  ;buddy[cpu_current()].p32=ptr  ;break;
+    case 64  :ptr->type=64  ;buddy[cpu_current()].p64=ptr  ;break;
+    case 128 :ptr->type=128 ;buddy[cpu_current()].p128=ptr ;break;
+    case 256 :ptr->type=256 ;buddy[cpu_current()].p256=ptr ;break;
+    case 512 :ptr->type=512 ;buddy[cpu_current()].p512=ptr ;break;
+    case 1024:ptr->type=1024;buddy[cpu_current()].p1024=ptr;break;
+    case 2048:ptr->type=2048;buddy[cpu_current()].p2048=ptr;break;
+    case 4096:ptr->type=4096;buddy[cpu_current()].p4096=ptr;break;
+    }
+  }
+  else{
+    while(ptr->next!=NULL){
+      if(ptr->now<ptr->max)break;
+      ptr=ptr->next;
+    }
+  }
+  if(ptr->now>=ptr->max){//没有空闲页
+    struct page_t* tmp=ptr;
+    ptr = new_page();
+    ptr->prev=tmp;tmp->next=ptr;ptr->next=NULL;
+    ptr->now=0;ptr->max=7168/size;ptr->type=size;
+  }
+  debug("ptr=%x\n",ptr);
+  for(int i=0;i<ptr->max;i++){
+    int x=i/64;
+    uint64_t y=1<<(i%64);
+    if(((ptr->map[x])&y)==0){//找到页中空闲位置，计算地址
+    debug("i=%d\n",i);
+      ptr->map[x]|=y;
+      ptr->now++;
+      addr=(uintptr_t)ptr+1024+ptr->type*i;
+      if(size==2048)addr+=1024;
+      else if(size==4096)addr+=3072;
+      break;
+    }
+  }
+  debug("size=%d\taddr=%x\t%d\n",size,addr,addr);
+  return (void*)addr;
 }
 
 static void kfree(void *ptr) {
