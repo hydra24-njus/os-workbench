677138fa9abc93d4daa769b0ec76e702aadb686f
tracer-nju <tracer@nju.edu.cn>
2022-03-29 16:14:17 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:14:17 up 32 min,  1 user,  load average: 0.39, 0.73, 0.72

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -61,6 +61,10 @@ static inline int xchg(volatile int *addr,int newval){
   	       "cc");
   return result;
 }
-void spinlock_init(struct spinlock_t *lk);
-void lock(struct spinlock_t *lk);
-void unlock(struct spinlock_t *lk);
+
+typedef struct spinlock{
+  int locked;
+}spinlock_t;
+void spinlock_init(spinlock_t *lk);
+void lock(spinlock_t *lk);
+void unlock(spinlock_t *lk);
