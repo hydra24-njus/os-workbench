a78cc66725e16b07669934ab3bb027edf5b4295f
tracer-nju <tracer@nju.edu.cn>
2022-03-29 12:20:16 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:20:16 up 10:21,  1 user,  load average: 0.34, 0.38, 0.28

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -1,2 +1,15 @@
+#include"am.h"
 #include "thread.h"
-#include <glibc.h>
+
+typedef struct spinlock{
+  int locked;
+}spinlock_t;
+void spinlock_init(spinlock_t *lk);
+void lock(spinlock_t *lk);
+void unlock(spinlock_t *lk);
+
+#ifdef LOCAL_MACHINE
+  #define debug(...) printf(__VA_ARGS__)
+#else
+  #define debug(...)
+#endif
