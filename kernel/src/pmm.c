77354b1464710a0c6d65aa858f9b9123604d913f
tracer-nju <tracer@nju.edu.cn>
2022-03-29 19:13:22 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:13:22 up  3:31,  1 user,  load average: 0.57, 0.49, 0.54

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -138,8 +138,9 @@ static void kfree(void *ptr) {
   if(ptr==NULL)return;
   uintptr_t addr=(uintptr_t)ptr;
   if(addr>=heapend)return;
-  printf("free-ptr=%x\n",ptr);
+  printf("free-ptr=%x\t",ptr);
   page_t* header=(page_t*)(addr&(~(PAGE_SIZE-1)));
+  printf("haed-ptr=%x\n",header);
   addr=(addr%PAGE_SIZE);
   //if(header->type==2048||header->type==2048)addr-=1;
   header->map[addr]=0;
