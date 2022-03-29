c0eb00b2004a676d1801a3753aa99b99a631a512
tracer-nju <tracer@nju.edu.cn>
2022-03-29 20:24:47 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:24:47 up  4:43,  1 user,  load average: 0.63, 0.61, 0.54

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -22,7 +22,7 @@ static void test2(int tid) {
   for (int i = 1; i <= 10000; i++) {
     size_t a = (rand() % 2) ? rand() % 2000 + 1000 : rand() % 100 + 50;
     loc = pmm->alloc(a);
-    if(loc==NULL)assert(0);
+    //if(loc==NULL)assert(0);
     if (i % 5 == 0) pmm->free(loc);
   }
 }
