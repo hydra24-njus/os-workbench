9b66475a1d46aa2ee9dc0b1d36efebcfa498abe3
tracer-nju <tracer@nju.edu.cn>
2022-03-29 16:01:07 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:01:07 up 19 min,  1 user,  load average: 1.14, 1.13, 0.66

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -1,15 +1,51 @@
-#include"am.h"
-#include "thread.h"
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct spinlock{
-  int locked;
-}spinlock_t;
-void spinlock_init(spinlock_t *lk);
-void lock(spinlock_t *lk);
-void unlock(spinlock_t *lk);
+//摘出来pmm.c需要的am函数
+#ifdef TEST
+static inline int atomic_xchg(int *addr, int newval) {
+  int result;
+  // printf("test xchg\n");
+  asm volatile("lock xchg %0, %1"
+               : "+m"(*addr), "=a"(result)
+               : "1"(newval)
+               : "cc", "memory");
+  return result;
+}
 
-#ifdef LOCAL_MACHINE
-  #define debug(...) printf(__VA_ARGS__)
-#else
-  #define debug(...)
 #endif
+
+//为了能让test成功编译(提取了os.c)
+#define MODULE(mod)                           \
+  typedef struct mod_##mod##_t mod_##mod##_t; \
+  extern mod_##mod##_t *mod;                  \
+  struct mod_##mod##_t
+
+#define MODULE_DEF(mod)                \
+  extern mod_##mod##_t __##mod##_obj;  \
+  mod_##mod##_t *mod = &__##mod##_obj; \
+  mod_##mod##_t __##mod##_obj
+
+MODULE(os) {
+  void (*init)();
+  void (*run)();
+};
+
+MODULE(pmm) {
+  void (*init)();
+  void *(*alloc)(size_t size);
+  void (*free)(void *ptr);
+};
+
+typedef unsigned long int uintptr_t;
+typedef long int intptr_t;
+#define HEAP_SIZE (1 << 27)
+
+typedef struct {
+  void *start, *end;
+} Area;
+
+Area heap;
