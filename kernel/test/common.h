2542e4b7bc8d8b7f1cc2456df4235596654d95d4
tracer-nju <tracer@nju.edu.cn>
2022-03-29 16:46:48 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:46:48 up  1:05,  1 user,  load average: 0.78, 0.64, 0.59

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -23,6 +23,9 @@ static inline int atomic_xchg(int *addr, int newval) {
 #endif
 
 typedef unsigned long int uintptr_t;
+typedef uint8_t bool;
+define true 1
+define false 0
 #define HEAP_SIZE (1 << 27)
 
 typedef struct {
