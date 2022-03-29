fe860ada6179ce3c0d76db11a62596a744c645a2
tracer-nju <tracer@nju.edu.cn>
2022-03-29 18:37:35 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:37:35 up  2:55,  1 user,  load average: 0.88, 0.70, 0.67

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -48,7 +48,7 @@ int main() {
   clock_t start_time, end_time;
   // start_time = clock();
   pmm->init();
-  create(test4);
+  create(test5);
   join();
   end_time = clock();  // 结束时间
   /* 计算得出程序运行时间, 并将其输出到屏幕 */
