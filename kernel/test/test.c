d19c162446b4006ee850cdf6f425a556712a4540
tracer-nju <tracer@nju.edu.cn>
2022-03-30 01:28:16 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:28:16 up  9:46,  1 user,  load average: 0.44, 0.25, 0.19

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -1,10 +1,56 @@
 #include "common.h"
-static void entry(int tid) { pmm->alloc(128); }
-static void goodbye()      { printf("End.\n"); }
+#include <thread.h>
+
+static void test1(int tid) {
+  void* loc;
+  for (int i = 1; i <= 2000; i++) {
+    loc = pmm->alloc(5000 + (i * 1000) % 5000);
+    // if (i % 5 == 0) pmm->free(loc);
+  }
+}
+
+static void test2(int tid) {
+  void* loc;
+  for (int i = 1; i <= 10000; i++) {
+    size_t a = (rand() % 2) ? rand() % 2000 + 1000 : rand() % 100 + 50;
+    loc = pmm->alloc(a);
+    // if (i % 5 == 0) pmm->free(loc);
+  }
+}
+
+static void test3(int tid) {
+  void* loc;
+  for (int i = 1; i <= 10000; i++) {
+    loc = pmm->alloc(5000 + (i * 1000) % 5000);
+    pmm->free(loc);
+  }
+}
+
+static void test4(int tid) {
+  void* loc;
+  for (int i = 1; i <= 10000; i++) {
+    size_t a = (rand() % 2) ? rand() % 2000 + 1000 : rand() % 100 + 50;
+    loc = pmm->alloc(a);
+    if (i % 2 == 0) pmm->free(loc);
+  }
+}
+
+static void test5(int tid) {
+  void* loc;
+  for (int i = 1; i <= 10000; i++) {
+    size_t a = rand() % 128 + 5;
+    loc = pmm->alloc(a);
+    if (i % 5 == 0) pmm->free(loc);
+  }
+}
 int main() {
+  clock_t start_time, end_time;
+  // start_time = clock();
   pmm->init();
-  for (int i = 0; i < 4; i++)
-    create(entry);
-  join(goodbye);
+  create(test5);
+  join();
+  end_time = clock();  // 结束时间
+  /* 计算得出程序运行时间, 并将其输出到屏幕 */
+  printf("%lf\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
 }
 
