065efd0667b63ea01560511a9fe5d63da2d95eda
tracer-nju <tracer@nju.edu.cn>
2022-03-29 16:16:34 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:16:34 up 34 min,  1 user,  load average: 0.73, 0.72, 0.72

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -145,6 +145,9 @@ static void kfree(void *ptr) {
   return;
 }
 
+
+#ifndef TEST
+// 框架代码中的 pmm_init (在 AbstractMachine 中运行)
 static void pmm_init() {
   //init
   spinlock_init(&biglock);
@@ -154,6 +157,17 @@ static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 }
+#else
+// 测试代码的 pmm_init ()
+static void pmm_init() {
+  char *ptr  = malloc(HEAP_SIZE);
+  heap.start = ptr;
+  heap.end   = ptr + HEAP_SIZE;
+  heaptr=heap.start;
+  heapend=heap.end;
+  printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
+}
+#endif
 
 MODULE_DEF(pmm) = {
   .init  = pmm_init,
