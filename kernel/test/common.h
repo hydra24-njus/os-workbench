342c142fc7fdbc4e36426af3d1fcbe7cef2d6eb0
tracer-nju <tracer@nju.edu.cn>
2022-03-29 16:04:00 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:04:00 up 22 min,  1 user,  load average: 0.93, 1.10, 0.72

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -49,3 +49,24 @@ typedef struct {
 } Area;
 
 Area heap;
+
+struct spinlock_t{
+  int locked;
+};
+static inline int xchg(volatile int *addr,int newval){
+  int result;
+  asm volatile("lock;xchgl %0,%1":
+  	       "+m"(*addr),"=a"(result):
+  	       "1"(newval):
+  	       "cc");
+  return result;
+}
+void spinlock_init(struct spinlock_t *lk){
+  lk->locked=0;
+}
+void lock(struct spinlock_t *lk){
+  while(xchg(&lk->locked,1)!=0);
+}
+void unlock(struct spinlock_t *lk){
+  asm volatile("movl $0,%0":"+m"(lk->locked):);
+}
