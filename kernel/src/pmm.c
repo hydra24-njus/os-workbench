e1ec3e4c489ccdb582ee7535d53fcc5374fe2458
tracer-nju <tracer@nju.edu.cn>
2022-03-29 16:40:23 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:40:23 up 58 min,  1 user,  load average: 0.35, 0.38, 0.52

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -8,26 +8,23 @@ spinlock_t biglock;
 
 //数据结构
 enum{
-  p16=0,p32,p64,p128,p256,p512,p1024,p2048,p4096
+  p8=0,p16,p32,p64,p128,p256,p512,p1024,p2048,p4096
 };
 enum{
   FREE=0,FULL
 };
 struct cpu_t{
   void* type[10][2];
-  spinlock_t cpulock;
 }buddy[8];//smp<=8
-struct page_t{
-  union{
-    uint8_t size[PAGE_SIZE];
-    struct{
-      void* next;void* prev;
-      size_t type;
-      bool map[512];
-      int max,now,cur,cpu,bitype;
-    };
+typedef union{
+  uint8_t size[PAGE_SIZE];
+  struct{
+    void* prev;void* next;
+    int type,bitype;
+    int max,now,cur,cpu;
+    bool map[896];
   };
-};
+}page_t;
 
 //辅助函数
 void* sbrk(int size){
@@ -55,92 +52,15 @@ uintptr_t slowpath_alloc(size_t size){
   heapend=tmp;
   return heapend;
 }
-void add2full(struct page_t* ptr){
-  int cpu=ptr->cpu,btp=ptr->bitype;
-  buddy[cpu].type[btp][FREE]=ptr->next;
-  ptr->next=buddy[cpu].type[btp][FULL];
-  buddy[cpu].type[btp][FULL]=ptr;
-}
-void add2free(struct page_t* ptr){
-  int cpu=ptr->cpu,btp=ptr->bitype;
-  struct page_t* tmp=buddy[cpu].type[btp][FREE];
-  struct page_t* tmp2=ptr->prev;
-  tmp2->next=ptr->next;
-  while(tmp->next!=NULL)tmp=tmp->next;
-  tmp->next=ptr;ptr->next=NULL;
-}
+
 
 //static_assert(sizeof(bool)==1);
 static void *kalloc(size_t size1) {
-  uintptr_t addr=0;int cpu=cpu_current();
-  size_t size=power2(size1);
-  if(size>4096){
-  if(size>=(16<<20))return NULL;
-    lock(&biglock);
-    addr=slowpath_alloc(size);
-    debug("addr=%x\t%d\n",addr,addr);
-    unlock(&biglock);
-    return (void*)addr;
-  }
-  int bitsize=3;
-  while((1<<bitsize)!=size){bitsize++;}
-  bitsize-=4;
-  struct page_t* ptr=buddy[cpu].type[bitsize][FREE];
-  if (ptr == NULL){ //该cpu没有页
-    lock(&biglock);
-    ptr = sbrk(PAGE_SIZE);
-    unlock(&biglock);
-    if(ptr==0){
-      addr=0;
-      goto ret;
-    }
-    ptr->cpu=cpu;
-    ptr->type=size;
-    ptr->bitype=bitsize;
-    buddy[cpu].type[bitsize][FREE]=ptr;
-    ptr->next=NULL;ptr->prev=NULL;
-    ptr->now=0;ptr->max=DATA_SIZE/size;ptr->cur=0;
-  }
-  for(int i=0;i<ptr->max;i++){
-    int j=(i+ptr->cur)%ptr->max;
-    if((ptr->map[j])==false){//找到页中空闲位置，计算地址
-      ptr->map[j]=true;
-      ptr->now++;ptr->cur=(j+1)%ptr->max;
-      addr=(uintptr_t)ptr+1024+ptr->type*j;
-      if(size==2048)addr+=1024;
-      else if(size==4096)addr+=3072;
-      if(ptr->now==ptr->max)add2full(ptr);
-      break;
-    }
-  }
-  ret:
-  debug("%x\tsize=%d\taddr=%x\t%d\t%d\n",ptr,size,addr,addr,ptr->now);
-  return (void*)addr;
+  return NULL;
 }
 
 static void kfree(void *ptr) {
-  uintptr_t addr=(uintptr_t)ptr;
-  if(addr>=heapend)return;
-  struct page_t* header=(struct page_t*)(addr-addr%PAGE_SIZE);//考虑位操作优化
-  addr=(addr%PAGE_SIZE);
-  if(header->type==2048){//2048 4096 6144
-    int i=addr/2048;
-    header->map[i-1]=false;
-    header->cur=i-1;
-  }
-  else if(header->type==4096){
-    int i=addr/4096;
-    header->map[i-1]=false;
-    header->cur=i-1;
-  }
-  else{
-    int i=(addr-1024)/header->type;
-    header->map[i]=false;
-    header->cur=i;
-  }
-  if(header->now==header->max)add2free(header);
-  header->now--;
-  return;
+  
 }
 
 static void pmm_init() {
