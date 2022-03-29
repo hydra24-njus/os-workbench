518520d671d227d527f42b428957010b68c5e25f
tracer-nju <tracer@nju.edu.cn>
2022-03-29 19:55:24 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:55:24 up  4:13,  1 user,  load average: 0.81, 0.83, 0.85

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -51,7 +51,7 @@ unsigned int bitpos(size_t size){
 }
 void add2full(page_t* ptr){
   if(ptr->state==FULL)return;
-  debug("add to full(%x):",ptr);
+  debug("add to full(%x):\nfull:",ptr);
   size_t bitype=ptr->bitype,cpu=ptr->cpu;
   buddy[cpu].type[bitype][FREE]=ptr->next;
   ptr->next=buddy[cpu].type[bitype][FULL];
@@ -68,7 +68,7 @@ void add2full(page_t* ptr){
 }
 void add2free(page_t* ptr){
   if(ptr->state==FREE)return;
-  debug("add to free(%x):",ptr);
+  debug("add to free(%x):\nfull:",ptr);
   size_t bitype=ptr->bitype,cpu=ptr->cpu;
   page_t* tmp=ptr->prev;
   //debug("(tmp=%x)",tmp);
