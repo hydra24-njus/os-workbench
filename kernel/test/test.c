3d2d09b5d3d3bbd6f286a6d2e16940e43232243d
tracer-nju <tracer@nju.edu.cn>
2022-03-29 18:39:16 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:39:16 up  2:57,  1 user,  load average: 0.86, 0.75, 0.69

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -13,7 +13,6 @@ static void test2(int tid) {
   void* loc;
   for (int i = 1; i <= 10000; i++) {
     size_t a = (rand() % 2) ? rand() % 2000 + 1000 : rand() % 100 + 50;
-    printf("a=%d\n",a);
     loc = pmm->alloc(a);
     // if (i % 5 == 0) pmm->free(loc);
   }
@@ -48,7 +47,7 @@ int main() {
   clock_t start_time, end_time;
   // start_time = clock();
   pmm->init();
-  create(test2);
+  create(test5);
   join();
   end_time = clock();  // 结束时间
   /* 计算得出程序运行时间, 并将其输出到屏幕 */
