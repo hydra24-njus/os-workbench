b832efd4f98460e01ff6e0f620816e063bd439fa
tracer-nju <tracer@nju.edu.cn>
2022-03-30 22:48:19 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:48:19 up 10:41,  1 user,  load average: 0.54, 0.59, 0.69

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,61 +1,5 @@
 #include <common.h>
 
-void test1(int tid) {
-  printf("start1\n");
-  for (int i = 1; i <= 2000; i++) {
-    void* loc=pmm->alloc(10000);
-    printf("loc=%p\n",loc);
-    // if (i % 5 == 0) pmm->free(loc);
-  }
-  printf("end1\n");
-}
-
-void test2(int tid) {
-  printf("start2\n");
-  void* loc;
-  for (int i = 1; i <= 10000; i++) {
-    size_t a = (rand() % 2) ? rand() % 2000 + 1000 : rand() % 100 + 50;
-    loc = pmm->alloc(a);
-    printf("loc=%p\n",loc);
-    if(loc==NULL)assert(0);
-    if (i % 5 == 0) pmm->free(loc);
-  }
-  printf("end2\n");
-}
-
-void test3(int tid) {
-  void* loc;
-  for (int i = 1; i <= 10000; i++) {
-    loc = pmm->alloc(5000 + (i * 1000) % 5000);
-    printf("loc=%p\n",loc);
-    pmm->free(loc);
-  }
-  printf("end3\n");
-}
-
-void test4(int tid) {
-  void* loc;
-  for (int i = 1; i <= 10000; i++) {
-    size_t a = (rand() % 2) ? rand() % 2000 + 1000 : rand() % 100 + 50;
-    loc = pmm->alloc(a);
-    printf("loc=%p\n",loc);
-    if (i % 2 == 0) pmm->free(loc);
-  }
-  printf("end4\n");
-}
-
-void test5(int tid) {
-  void* loc;
-  for (int i = 1; i <= 10000; i++) {
-    size_t a = rand() % 128 + 5;
-    loc = pmm->alloc(a);
-    printf("loc=%p\n",loc);
-    if (i % 5 == 0) pmm->free(loc);
-  }
-  printf("end5\n");
-}
-
-
 
 static void os_init() {
   pmm->init();
@@ -64,12 +8,6 @@ static void os_run() {
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     //putch(*s == '*' ? '0' + cpu_current() : *s);
   }
-  test1(1);
-  test2(1);
-  test3(1);
-  test4(1);
-  test5(1);
-  printf("end\n");
   while (1) ;
 }
 
