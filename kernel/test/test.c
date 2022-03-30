241b5acc87dcd70829ba15b47d1e1ae2f34fc264
tracer-nju <tracer@nju.edu.cn>
2022-03-30 19:27:18 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:27:18 up  7:20,  1 user,  load average: 0.86, 0.51, 0.44

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -2,7 +2,7 @@
 #include <thread.h>
 
 static void test0(int tid) {
-  printf("alloc=%lx\n",pmm->alloc(16<<20));
+  printf("alloc=%lx\n",pmm->alloc(16<<19));
   printf("alloc pass\n");
   pmm->free(0);
 }
