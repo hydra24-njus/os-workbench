111fda616abe654fdc51c4295827db2f497af0cd
tracer-nju <tracer@nju.edu.cn>
2022-03-30 18:54:34 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:54:34 up  6:47,  1 user,  load average: 0.49, 0.57, 0.50

--- kernel/src/buddysystem.c
+++ kernel/src/buddysystem.c
@@ -77,6 +77,7 @@ void* buddy_alloc(size_t size){
     }
     else{
         uintptr_t addr=(uintptr_t)buddy_alloc(size<<1);
+        printf("addr=%x\n",addr);
         page_t* tmp=(page_t*)addr2map(addr);
         printf("tmp->size=%d\n",tmp->size);
         page_t* tmp2=(page_t*)addr2map(addr+size);
