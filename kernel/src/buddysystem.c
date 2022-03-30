7f5b3de0fd99298bc7ef71b4af8a5245be26f382
tracer-nju <tracer@nju.edu.cn>
2022-03-30 18:47:12 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:47:12 up  6:40,  1 user,  load average: 0.80, 0.56, 0.43

--- kernel/src/buddysystem.c
+++ kernel/src/buddysystem.c
@@ -37,7 +37,7 @@ void print_mem_tree(){
     printf("%d:",i);
     while(tmp!=NULL){
         uintptr_t num=map2addr((uintptr_t)tmp);
-        printf("%x(%d)->",num,(tmp->size)>>10);
+        printf("%x(%d)->",num,tmp->size);
         tmp=tmp->next;
     }
     printf("\n");
