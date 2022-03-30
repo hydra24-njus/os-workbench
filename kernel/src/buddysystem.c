b448ed11bf33886f31e235e0d836a9d7117a6b4e
tracer-nju <tracer@nju.edu.cn>
2022-03-30 19:10:22 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:10:22 up  7:03,  1 user,  load average: 0.22, 0.27, 0.39

--- kernel/src/buddysystem.c
+++ kernel/src/buddysystem.c
@@ -52,7 +52,7 @@ void print_mem_tree(){
 
 
 void buddy_init(uintptr_t heapstart,uintptr_t heapend){
-    printf("%d\n",sizeof(tree));
+    printf("%lx\n",heapstart);
     tree_head=(tree*)heapstart;heapstart+=sizeof(tree);
     if(heapstart%M16!=0)heapstart=heapstart+M16-heapstart%M16;
     heapend=(heapend>>24)<<24;
