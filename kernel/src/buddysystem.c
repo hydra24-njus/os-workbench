315ff5b2e46287090886f12276f3c03b1b65ce20
tracer-nju <tracer@nju.edu.cn>
2022-03-30 18:59:04 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:59:04 up  6:52,  1 user,  load average: 0.37, 0.52, 0.51

--- kernel/src/buddysystem.c
+++ kernel/src/buddysystem.c
@@ -26,7 +26,9 @@ uintptr_t map2addr(uintptr_t map){
 }
 uintptr_t addr2map(uintptr_t addr){
     printf("addr=%x\n",addr);
-    uintptr_t num=(addr-HEAPSTART)/(64<<10);
+    uintptr_t num=(addr-HEAPSTART);
+    printf("num=%d\n",num);
+    num=num/(64<<10);
     printf("num=%d\n",num);
     num=num*sizeof(page_t)+(uintptr_t)tree_head->units;
     printf("num=%d\n",num);
