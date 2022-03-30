ad78ea60a108c1c32fec64582abe26c4ba4a4b22
tracer-nju <tracer@nju.edu.cn>
2022-03-30 17:46:34 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:46:34 up  5:39,  1 user,  load average: 0.36, 0.23, 0.27

--- kernel/src/buddysystem.c
+++ kernel/src/buddysystem.c
@@ -3,7 +3,7 @@
 #include <lock.h>
 #define M16 (1<<24)
 
-typedef struct pagecontrol{
+typedef struct{
     int size;
     int type;
     int state;
@@ -24,10 +24,8 @@ static tree* tree_head;
 void buddy_init(uintptr_t heapstart,uintptr_t heapend){
     printf("%d\n",sizeof(tree));
     tree_head=(tree*)heapstart;heapstart+=sizeof(tree);
-    //uintptr_t page64k=heapstart;
     if(heapstart%M16!=0)heapstart=heapstart+M16-heapstart%M16;
     heapend=(heapend>>24)<<24;
     printf("%x~%x,size= %u MB\n",heapstart,heapend,(heapend-heapstart)>>20);
-    //int i=0;tree_head->units[0]=
 
 }
