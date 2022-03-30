a477704efc79c9970bf6edc7b95f877bc7f4ff20
tracer-nju <tracer@nju.edu.cn>
2022-03-30 18:23:38 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:23:38 up  6:17,  1 user,  load average: 0.48, 0.54, 0.44

--- kernel/src/buddysystem.c
+++ kernel/src/buddysystem.c
@@ -2,7 +2,7 @@
 #include <buddysystem.h>
 #include <lock.h>
 #define M16 (1<<24)
-
+#define HEAPSTART 0x1000000
 typedef struct{
     int size;
     int state;
@@ -17,9 +17,34 @@ typedef union{
     };
     uint8_t data[1<<18];
 }tree;//2^18 Byte
-
-
 static tree* tree_head;
+
+uintptr_t map2addr(uintptr_t map){
+    uintptr_t num=(map-(uintptr_t)tree_head->units)/sizeof(page_t);
+    num=HEAPSTART+num*(64<<10);
+    return num;
+}
+uintptr_t addr2map(uintptr_t addr){
+    uintptr_t num=(addr-HEAPSTART)/(64<<10);
+    num=num*sizeof(page_t)+(uintptr_t)tree_head->units;
+    return num;
+}
+
+
+void print_mem_tree(){
+    for(int i=0;i<MAX_ORDER;i++){
+    page_t* tmp=tree_head->free_list[i];
+    printf("%d:",i);
+    while(tmp!=NULL){
+        uintptr_t num=map2addr((uintptr_t)tmp);
+        printf("%x->",num);
+        tmp=tmp->next;
+    }
+    printf("\n");
+    }
+}
+
+
 void buddy_init(uintptr_t heapstart,uintptr_t heapend){
     printf("%d\n",sizeof(tree));
     tree_head=(tree*)heapstart;heapstart+=sizeof(tree);
@@ -35,15 +60,21 @@ void buddy_init(uintptr_t heapstart,uintptr_t heapend){
         tree_head->units[i*256].next=&tree_head->units[(i+1)*256];
     }
     tree_head->units[(maxpage-1)*256].next=NULL;
-    for(int i=0;i<MAX_ORDER;i++){
-    page_t* tmp=tree_head->free_list[i];
-    printf("%d:",i);
-    while(tmp!=NULL){
+    print_mem_tree();
+}
+
+void* buddy_alloc(size_t size){
+    int i=0;while((1<<i)<size)i++;
+    if(tree_head->free_list[i]!=NULL){
+        page_t* tmp=tree_head->free_list[i];
+        tree_head->free_list[i]=tmp->next;
+        tmp->next=NULL;
         uintptr_t num=((uintptr_t)tmp-(uintptr_t)tree_head->units)/sizeof(page_t);
-        num=heapstart+num*(64<<10);
-        printf("%x->",num);
-        tmp=tmp->next;
+        num=0x1000000+num*(64<<10);
+        return (void*)num;
     }
-    printf("\n");
+    else{
+        buddy_alloc(size<<1);
     }
+    return NULL;
 }
