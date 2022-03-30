1f998377cdff2ea33065851742adc108303b6901
tracer-nju <tracer@nju.edu.cn>
2022-03-30 16:32:56 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:32:56 up  4:26,  1 user,  load average: 1.45, 0.93, 0.80

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -1,7 +1,31 @@
+#ifndef TEST
 #include <kernel.h>
 #include <klib.h>
 #include <klib-macros.h>
+#else
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <kernel.h>
 
+//摘出来pmm.c需要的am函数
+static inline int atomic_xchg(int *addr, int newval) {
+  int result;
+  // printf("test xchg\n");
+  asm volatile("lock xchg %0, %1"
+               : "+m"(*addr), "=a"(result)
+               : "1"(newval)
+               : "cc", "memory");
+  return result;
+}
+#endif
+
+typedef unsigned long int uintptr_t;
+#define HEAP_SIZE (1 << 27)
+#define cpu_current() 1
+#endif
 
 #ifdef LOCAL_MACHINE
   #define debug(...) printf(__VA_ARGS__)
