aee2a7083beb1b643a1898c5a20e481646ad1e98
tracer-nju <tracer@nju.edu.cn>
2022-03-30 19:57:11 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:57:11 up  7:50,  1 user,  load average: 1.01, 0.47, 0.34

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -28,6 +28,7 @@ unsigned int bitpos(size_t size){
 static void *kalloc(size_t size) {
   size=power2(size);
   if(size>(16<<20))return NULL;
+  printf("start buddy_alloc\n");
   return buddy_alloc(size);
   return NULL;
 }
