7dffb524872b07984f76a82d8620df89efb80662
tracer-nju <tracer@nju.edu.cn>
2022-04-20 17:16:16 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:16:16 up  1:23,  1 user,  load average: 1.66, 1.11, 0.72

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,8 +1,24 @@
 #include <common.h>
 
+typedef struct irq_handler{
+  int seq;int event;
+  handler_t handler;
+  struct irq_handler* next;
+}irq_handler_t;
+void irq_guard_fun(){
+  debug("should not reach here.\n");
+  assert(0);
+}
+static irq_handler_t irq_guard={
+  .seq=INT32_MIN,
+  .event=-1,
+  .handler=(handler_t)irq_guard_fun,
+  .next=NULL
+};
+
 static void os_init() {
   pmm->init();
-  //kmt->init();
+  kmt->init();
   dev->init();
 }
 static void os_run() {
@@ -12,10 +28,32 @@ static void os_run() {
   while (1) ;
 }
 Context *os_trap(Event ev, Context *context){
-  return context;
+  Context *next=NULL;
+  for(irq_handler_t* handler_now=&irq_guard;handler_now!=NULL;handler_now=handler_now->next){
+    if(handler_now->event==EVENT_NULL||handler_now->event==ev.event){
+      Context* r=handler_now->handler(ev,context);
+      panic_on(r&&next,"returning multiple contexts");
+      if(r)next=r;
+    }
+  }
+  panic_on(!next,"returning NULL context");
+  //panic_on(sane_context(next),"returning to invalid context");
+  return next;
 }
 void os_on_irq(int seq, int event, handler_t handler){
-  return;
+  irq_handler_t *h=pmm->alloc(sizeof(irq_handler_t));
+  h->event=event;
+  h->seq=seq;
+  h->handler=handler;
+  h->next=NULL;
+  irq_handler_t* prev=&irq_guard;irq_handler_t* p=irq_guard.next;
+  while(p!=NULL){
+    if(p->seq>seq)break;
+    prev=p;
+    p=p->next;
+  }
+  prev->next=h;
+  h->next=p;
 }
 MODULE_DEF(os) = {
   .init = os_init,
