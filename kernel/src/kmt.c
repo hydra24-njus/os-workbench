f541510e5bcbae69e65d41a44e631fedbea06f26
tracer-nju <tracer@nju.edu.cn>
2022-04-20 18:12:42 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:12:42 up  2:20,  1 user,  load average: 1.88, 1.24, 1.01

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -4,12 +4,12 @@ static Context *kmt_context_save(Event ev,Context *context){
   return NULL;
 }
 static Context *kmt_schedule(Event ev,Context *context){
-    debug("schedule\n");
+  //debug("schedule\n");
   return context;
 }
 void kmt_init(){
   os->on_irq(INT32_MIN+1,EVENT_NULL,kmt_context_save);
-  debug("save\n");
+  //debug("save\n");
   os->on_irq(INT32_MAX,EVENT_NULL,kmt_schedule);
   return;
 }
