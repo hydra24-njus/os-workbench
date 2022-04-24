c7808af8f0989cb6610d4656d198bf751dcb6c30
tracer-nju <tracer@nju.edu.cn>
2022-04-24 17:20:01 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:20:01 up  2:45,  1 user,  load average: 0.76, 0.62, 0.58

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -53,7 +53,7 @@ static int create(task_t *task,const char *name,void (*entry)(void *arg),void *a
   task->entry=entry;
   task->next=header->next;
   header->next=task;
-  Area stack={&task->context,&task+sizeof(task_t)-sizeof(uint32_t)};
+  Area stack={&task->context,&task+sizeof(task_t)};
   task->context=kcontext(stack,entry,arg);
   return 0;
 }
