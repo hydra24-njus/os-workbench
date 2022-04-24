726cad98799251100817cbfc4a4ea7c5ddafbfeb
tracer-nju <tracer@nju.edu.cn>
2022-04-24 15:05:19 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:05:19 up 30 min,  1 user,  load average: 0.87, 0.54, 0.33

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -23,7 +23,7 @@ int create(task_t *task,const char *name,void (*entry)(void *arg),void *arg){
 void teardown(task_t *task){
   return;
 }
-void spin_init(spinlock_t *lk){
+void spin_init(spinlock_t *lk,const char *name){
   return;
 }
 void spin_lock(spinlock_t *lk){
@@ -32,7 +32,7 @@ void spin_lock(spinlock_t *lk){
 void spin_unlock(spinlock_t *lk){
 
 }
-void sme_init(sem_t *sem,const char *name,int value){
+void sem_init(sem_t *sem,const char *name,int value){
 
 }
 void sem_wait(sem_t *sem){
@@ -44,4 +44,12 @@ void sem_signal(sem_t *sem){
 MODULE_DEF(kmt) = {
  // TODO
  .init=kmt_init,
+ .create=create,
+ .teardown=teardown,
+ .spin_init=spin_init,
+ .spin_lock=spin_lock,
+ .spin_unlock=spin_unlock,
+ .sem_init=sem_init,
+ .sem_wait=sem_wait,
+ .sem_signal=sem_signal
 };
