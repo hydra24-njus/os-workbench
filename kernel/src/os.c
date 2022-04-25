c19e1137433ab5d96eed0af01984c3830e9f8db6
tracer-nju <tracer@nju.edu.cn>
2022-04-25 11:22:40 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:22:40 up 46 min,  1 user,  load average: 0.57, 0.69, 0.61

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -26,9 +26,9 @@ void fun2(){
 static void os_init() {
   pmm->init();
   kmt->init();
-  //kmt->create(pmm->alloc(sizeof(task_t)),"fun1",fun1,NULL);
-  //kmt->create(pmm->alloc(sizeof(task_t)),"fun2",fun2,NULL);
-  dev->init();
+  kmt->create(pmm->alloc(sizeof(task_t)),"fun1",fun1,NULL);
+  kmt->create(pmm->alloc(sizeof(task_t)),"fun2",fun2,NULL);
+  //dev->init();
   debug("init finished.\n");
 }
 static void os_run() {
