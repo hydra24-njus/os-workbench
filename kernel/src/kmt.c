8e04ed650359203ca552d31654dbd97f70d307da
tracer-nju <tracer@nju.edu.cn>
2022-04-25 14:55:57 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:55:57 up 10 min,  1 user,  load average: 0.52, 0.68, 0.40

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -51,7 +51,7 @@ void kmt_init(){
     task->entry=NULL;
     task->next=NULL;
     cpu_header[i]=task;
-    current=cpu_header[i];
+    cpu_currents[i]=cpu_header[i];
     Area stack={&task->context+1,task+1};
     task->context=kcontext(stack,NULL,NULL);
   }
