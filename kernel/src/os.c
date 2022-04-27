67ae64aa2d043175e23b95d2a1d0b314e675390e
tracer-nju <tracer@nju.edu.cn>
2022-04-27 15:29:44 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:29:44 up  2:07,  1 user,  load average: 1.21, 1.43, 1.27

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -35,7 +35,7 @@ static void os_init() {
   kmt->init();
   kmt->spin_init(&kmt_lock,"中断处理");
 
-  kmt->sem_init(&empty, "empty", 0);  // 缓冲区大小为 5
+  kmt->sem_init(&empty, "empty", 2);  // 缓冲区大小为 5
   kmt->sem_init(&fill,  "fill",  0);
   for (int i = 0; i < 4; i++) // 4 个生产者
     kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
