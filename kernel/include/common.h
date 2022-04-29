cd89376d290cd5f27bc7e370267938bbf02fbea1
tracer-nju <tracer@nju.edu.cn>
2022-04-29 12:28:50 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:28:50 up 15 min,  1 user,  load average: 0.10, 0.36, 0.32

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -59,4 +59,37 @@ typedef unsigned long int uintptr_t;
   #define debug(...)
   #define r_panic_on(...)
 #endif
+#define MAGIC 0x114514
+enum{
+  READY=0,RUNNING,SLEEPING,IDLE
+};
+struct task {
+  // TODO
+  union{
+    struct {
+    int status;
+    struct semaphore *sem;
+    const char *name;
+    struct task *next;
+    void (*entry)(void*);
+    Context   *context;
+  };
+  uint8_t stack[4096];
+  };
+};
 
+struct spinlock {
+  // TODO
+  int locked;
+  int intr;//cli/sti
+  char* name;
+  int cpu;
+};
+
+struct semaphore {
+  // TODO
+  int value;int count;
+  struct spinlock lock;
+  char *name;
+  int cpu;
+};
