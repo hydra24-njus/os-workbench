7aa8e2ee0d26210bb0ecc6126b8063ba77d178cb
tracer-nju <tracer@nju.edu.cn>
2022-04-29 12:23:24 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:23:24 up 10 min,  1 user,  load average: 0.32, 0.48, 0.31

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,9 +1,12 @@
 #include <common.h>
 #include <klib.h>
+
 int main() {
   ioe_init();
   cte_init(os->trap);
   os->init();
+  task_t *p=NULL;
+  if(p)printf("1\n");
   mpe_init(os->run);
   return 1;
 }
