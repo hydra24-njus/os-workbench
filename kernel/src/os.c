6cdcac44437f2e6421a3f0d14947e1c4273aea56
tracer-nju <tracer@nju.edu.cn>
2022-05-03 18:36:43 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:36:43 up 4 min,  1 user,  load average: 0.53, 0.60, 0.29

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -18,6 +18,14 @@ static irq_handler_t irq_guard={
 void test_kmt(){
   debug("xxx\n");
 }
+#define P kmt->sem_wait
+#define V kmt->sem_signal
+sem_t empty,fill;
+void producer() { while (1) { P(&empty); putch('('); V(&fill);  } }
+void consumer() { while (1) { P(&fill);  putch(')'); V(&empty); } }
+void* task_alloc(){
+  return pmm->alloc(sizeof(task_t));
+}
 void fun(void *i){
     while (1) {
     printf("Thread-%s%d on CPU #%d\n", "func", i, cpu_current());
@@ -30,8 +38,13 @@ static void os_init() {
   kmt->init();
   kmt->spin_init(&kmt_lock,"中断处理");
   //for(uintptr_t i=0;i<32;i++)kmt->create(pmm->alloc(sizeof(task_t)),"func",fun,(void *)i);
-  dev->init();
-  debug("init finished.\n");
+  //dev->init();
+  kmt->sem_init(&empty, "empty", 5);  // 缓冲区大小为 5
+  kmt->sem_init(&fill,  "fill",  0);
+  for (int i = 0; i < 4; i++) // 4 个生产者
+    kmt->create(task_alloc(), "producer", producer, NULL);
+  for (int i = 0; i < 5; i++) // 5 个消费者
+    kmt->create(task_alloc(), "consumer", consumer, NULL);
 }
 static void os_run() {
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
