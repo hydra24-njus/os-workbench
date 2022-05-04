8976e1ed1cd40b9d25e2cd78924e1d03808beda0
tracer-nju <tracer@nju.edu.cn>
2022-05-05 00:54:04 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:54:04 up 31 min,  1 user,  load average: 0.22, 0.40, 0.54

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -1,11 +1,9 @@
 #include <os.h>
 task_t *cpu_currents[8];
-task_t *lasts[8];
 task_t *cpu_idle[8];
 task_t *cpu_header;
 spinlock_t tasklock;
 #define current cpu_currents[cpu_current()]
-#define last lasts[cpu_current()]
 #define idle cpu_idle[cpu_current()]
 static int ncli[8]={0};
 static int intena[8]={0};
@@ -53,11 +51,7 @@ static Context *kmt_context_save(Event ev,Context *context){
   debug("(%d)save\n",cpu_current());
   r_panic_on(current==NULL,"current==NULL");
   current->context=context;
-  if(last){
-    r_panic_on(last->status!=WAITING&&last->status!=IDLE,"error status:%d",last->status);
-    atomic_xchg(&last->status,READY);
-    last=NULL;
-  }
+  if(current->status==RUNNING)current->status=WAITING;
   return NULL;
 }
 static Context *kmt_schedule(Event ev,Context *context){
@@ -80,8 +74,6 @@ static Context *kmt_schedule(Event ev,Context *context){
     }
     if(p==NULL)p=idle;
   }
-  last=current;
-  atomic_xchg(&last->status,WAITING);
   current=p;
   debug("(%d)schedule:%s\n",cpu_current(),current->name);
   spin_unlock(&tasklock);
