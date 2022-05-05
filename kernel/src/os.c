b508d4643a6cf191712c91db4d099d0efaa8c924
tracer-nju <tracer@nju.edu.cn>
2022-05-05 22:10:52 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:10:52 up  1:01,  1 user,  load average: 0.17, 0.40, 0.82

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -20,25 +20,9 @@ static inline task_t *task_alloc() {
   return pmm->alloc(sizeof(task_t));
 }
 
-static void tty_reader(void *arg) {
-  device_t *tty = dev->lookup(arg);
-  char cmd[128], resp[128], ps[16];
-  snprintf(ps, 16, "(%s) $ ", arg);
-  while (1) {
-    tty->ops->write(tty, 0, ps, strlen(ps));
-    int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
-    cmd[nread] = '\0';
-    sprintf(resp, "tty reader task: got %d character(s).\n", strlen(cmd));
-    tty->ops->write(tty, 0, resp, strlen(resp));
-  }
-}
-
 static void os_init() {
   pmm->init();
   kmt->init();
-  dev->init();
-  kmt->create(task_alloc(), "tty_reader", tty_reader, "tty1");
-  kmt->create(task_alloc(), "tty_reader", tty_reader, "tty2");
 }
 static void os_run() {
   iset(true);
