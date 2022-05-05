fefe1ee0a05cd2788fe0245682dd006db7dd1f88
tracer-nju <tracer@nju.edu.cn>
2022-05-05 19:48:26 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:48:26 up 27 min,  1 user,  load average: 0.29, 0.50, 0.48

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -52,10 +52,9 @@ static void spin_unlock(spinlock_t *lk){
 static Context *kmt_context_save(Event ev,Context *context){
   debug("(%d)save\n",cpu_current());
   r_panic_on(current==NULL,"current==NULL");
-  r_panic_on(current->status!=RUNNING&&current->status!=IDLE&&current->status!=SLEEPING,"%d",current->status);
+  r_panic_on(current->status!=RUNNING&&current->status!=IDLE&&current->status!=SLEEPING,"current status error(%d)",current->status);
   if(current->status==RUNNING)current->status=ZOMBIE;
   else if(current->status==SLEEPING)current->status+=ZOMBIE;
-  r_panic_on(current->status<IDLE,"current status error(%d).",current->status);
   if(last){
     if(last->status!=IDLE){
     r_panic_on(last->status<ZOMBIE,"last status error(%d).",last->status);
@@ -87,6 +86,7 @@ static Context *kmt_schedule(Event ev,Context *context){
   last=current;
   current=p;
   current->status=RUNNING;
+  panic_on(current->status!=RUNNING,"in schedule");
   debug("(%d)schedule:%s\n",cpu_current(),current->name);
   return current->context;
 }
