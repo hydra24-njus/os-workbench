1dad428c0d2eaea9e6e7ad76c7767cf8a211cd54
tracer-nju <tracer@nju.edu.cn>
2022-05-05 19:51:29 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:51:29 up 30 min,  1 user,  load average: 1.63, 0.90, 0.62

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -38,12 +38,12 @@ static void os_init() {
   //dev->init();
   /*for (uintptr_t i = 0; i < 10; i++)
     kmt->create(task_alloc(), "func", fun, (void *)i);*/
-  kmt->sem_init(&empty, "empty", 5);  // 缓冲区大小为 5
+  /*kmt->sem_init(&empty, "empty", 5);  // 缓冲区大小为 5
   kmt->sem_init(&fill,  "fill",  0);
   for (int i = 0; i < 4; i++) // 4 个生产者
     kmt->create(task_alloc(), "producer", producer, NULL);
   for (int i = 0; i < 5; i++) // 5 个消费者
-    kmt->create(task_alloc(), "consumer", consumer, NULL);
+    kmt->create(task_alloc(), "consumer", consumer, NULL);*/
 }
 static void os_run() {
   iset(true);
