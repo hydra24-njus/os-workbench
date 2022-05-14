626d087c280cfd17df6c8b084fe22e460d700adb
tracer-nju <tracer@nju.edu.cn>
2022-05-14 19:37:27 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:37:27 up  4:16,  1 user,  load average: 1.27, 1.18, 1.06

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -2,6 +2,47 @@
 #include <syscall.h>
 
 #include "initcode.inc"
-
+void uproc_init(){
+  //vme_init(pmm->alloc,pmm->free);
+  return;
+}
+int kputc(task_t *task,char ch){
+  putch(ch);
+  return 0;
+}
+int fork(task_t *task){
+  return 0;
+}
+int wait(task_t *task,int *status){
+  return 0;
+}
+int exit(task_t *task,int status){
+  return 0;
+}
+int kill(task_t *task,int pid){
+  return 0;
+}
+void *mmap(task_t *task,void *addr,int length,int prot,int flags){
+  return NULL;
+}
+int getpid(task_t *task){
+  return 0;
+}
+int sleep(task_t *task,int seconds){
+  return 0;
+}
+int64_t uptime(task_t *task){
+  return 0;
+}
 MODULE_DEF(uproc) = {
+  .init=uproc_init,
+  .kputc=kputc,
+  .fork=fork,
+  .wait=wait,
+  .exit=exit,
+  .kill=kill,
+  .mmap=mmap,
+  .getpid=getpid,
+  .sleep=sleep,
+  .uptime=uptime
 };
