89b616a5e41f8914836f2cb328f34120da9732b8
201240105-Zhai Jingbo <201240105@smail.nju.edu.cn>
2022-05-14 21:23:48 +0800

    l3

--- user/init.c
+++ user/init.c
@@ -2,6 +2,21 @@
 
 int main() {
   // Example:
+  kputc('h');
+  kputc('e');
+  kputc('l');
+  kputc('l');
+  kputc('o');
+  kputc('\n');
+  while(1){
+    kputc('h');
+    kputc('e');
+    kputc('l');
+    kputc('l');
+    kputc('o');
+    kputc('\n');
+    sleep(1);
+  }
   // printf("pid = %d\n", getpid());
   return 0;
 }
