6a64b0fbc9adfe27f55d862cbcaa825112fb0700
tracer-nju <tracer@nju.edu.cn>
2022-05-16 01:25:55 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:25:55 up  1:26,  1 user,  load average: 2.52, 2.03, 1.59

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -61,7 +61,7 @@ int getpid(task_t *task){
 int sleep(task_t *task,int seconds){
   kmt->spin_lock(&tasklock);
   current->wakeuptime=io_read(AM_TIMER_UPTIME).us+1000000*seconds;
-  printf("iotime:%d\twakeuptime:%d\n",io_read(AM_TIMER_UPTIME).us/1000000,current->wakeuptime/1000000);
+  printf("iotime:%d\twakeuptime:%d\n",current->wakeuptime/1000000-seconds,current->wakeuptime/1000000);
   last=current;
   current->status=SLEEPING+ZOMBIE;
   kmt->spin_unlock(&tasklock);
