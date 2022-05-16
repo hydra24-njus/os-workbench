cae79bee1f2b39a295edf99f4a1cdaffae5a4537
tracer-nju <tracer@nju.edu.cn>
2022-05-16 21:45:57 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:45:57 up 3 min,  1 user,  load average: 2.01, 1.30, 0.54

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -43,7 +43,7 @@ static void os_init() {
   uproc->init();
   kmt->create(task_alloc(), "tty_reader", tty_reader, "tty1");
   kmt->create(task_alloc(), "tty_reader", tty_reader, "tty2");
-  for(int i=0;i<20;i++)kmt->create(task_alloc(),"ide_test",ide,NULL);
+  //for(int i=0;i<20;i++)kmt->create(task_alloc(),"ide_test",ide,NULL);
 }
 static void os_run() {
   iset(true);
