7c428c09e9db08b548038962cbc618720091d619
tracer-nju <tracer@nju.edu.cn>
2022-06-25 17:04:02 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:04:02 up  2:37,  1 user,  load average: 0.76, 0.54, 0.44

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -177,9 +177,9 @@ int main(int argc, char *argv[]) {
     strcat(tmp_path,result[i]);
     struct bmp_header *bmp_fp=(struct bmp_header*)(data_start+first_clus[i]*clus_sz);
     FILE *bmp_tmp_file=fopen(tmp_path,"a");
-    assert(0);
     fwrite(bmp_fp,sizeof(struct bmp_header),1,bmp_tmp_file);
     struct bmp_infomation_header *bmp_ip=(struct bmp_infomation_header*)(bmp_fp+1);
+    assert(0);
     fwrite(bmp_ip,sizeof(struct bmp_infomation_header),1,bmp_tmp_file);
     uintptr_t img_start=(uintptr_t)(bmp_fp+bmp_fp->offset);
     if(bmp_ip->img_size>clus_sz-sizeof(struct bmp_header)-sizeof(struct bmp_infomation_header)){
