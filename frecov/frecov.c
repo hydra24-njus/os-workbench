53eda74e3f7db9681a876320321427249948f1ba
tracer-nju <tracer@nju.edu.cn>
2022-06-25 17:07:40 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:07:40 up  2:41,  1 user,  load average: 0.54, 0.64, 0.51

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -185,6 +185,7 @@ int main(int argc, char *argv[]) {
     if(bmp_ip->img_size>clus_sz-sizeof(struct bmp_header)-sizeof(struct bmp_infomation_header)){
       //多个簇
       //continue;
+      fwrite((void*)img_start,clus_sz-sizeof(struct bmp_header)-sizeof(struct bmp_infomation_header),1,bmp_tmp_file);
     }
     else{
       fwrite((void*)img_start,bmp_ip->img_size,1,bmp_tmp_file);
