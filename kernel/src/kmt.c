98e4c4d27cb6a37e9b110c096cfb3e2298346659
tracer-nju <tracer@nju.edu.cn>
2022-06-26 17:02:50 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:02:50 up  6:02,  1 user,  load average: 1.47, 1.05, 0.74

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -82,6 +82,7 @@ static Context *kmt_schedule(Event ev,Context *context){
     }
   }
   if(p==NULL||p==current)p=idle;
+  current->status=READY;
   current=p;
   if(current!=idle)current->status=RUNNING;
   r_panic_on(current->status!=RUNNING&&current->status!=IDLE,"in schedule,%d",current->status);
