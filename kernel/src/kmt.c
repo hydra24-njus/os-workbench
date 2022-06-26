28d76dd27f2297477786da5ad98eb7d956efe655
tracer-nju <tracer@nju.edu.cn>
2022-06-26 16:54:57 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:54:57 up  5:54,  1 user,  load average: 0.64, 0.62, 0.55

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -64,8 +64,8 @@ static Context *kmt_schedule(Event ev,Context *context){
     panic_on(p->status==DEAD,"DEAD task in lint-table");
     if(p->status==SLEEPING){
       if(p->wakeuptime!=0){
-        printf("awake\n");
         if(io_read(AM_TIMER_UPTIME).us>=p->wakeuptime){
+        printf("awake\n");
           p->status=READY;
         }
       }
