178e73a4efc5de9307ca384a6a1eb0c36f64530c
tracer-nju <tracer@nju.edu.cn>
2022-06-26 17:02:13 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:02:13 up  6:01,  1 user,  load average: 2.03, 1.06, 0.73

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -84,13 +84,10 @@ int getpid(task_t *task){
   return 0;
 }
 int sleep(task_t *task,int seconds){
-  kmt->spin_lock(&tasklock);
   uint64_t iotime=io_read(AM_TIMER_UPTIME).us;
-  current->wakeuptime=iotime+1000000*seconds;
+  uint64_t wakeuptime=iotime+1000000*seconds;
   debug("[%d]uptime:%d\twakeuptime:%d\n",cpu_current(),iotime/1000000,current->wakeuptime/1000000);
-  current->status=READY;
-  kmt->spin_unlock(&tasklock);
-  while(current->wakeuptime>io_read(AM_TIMER_UPTIME).us)yield();
+  while(wakeuptime>io_read(AM_TIMER_UPTIME).us)yield();
   return 0;
 }
 int64_t uptime(task_t *task){
