84f9bf6efd72019c1a74ea7ecd77991c5008a50a
tracer-nju <tracer@nju.edu.cn>
2022-06-27 18:39:58 +0800

    Linux hydra24-R7000P 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:39:58 up  1:02,  1 user,  load average: 0.56, 0.43, 0.51

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -179,7 +179,7 @@ static void teardown(task_t *task){
   printf("teardown(%d):",task->pid);
   if(tmp==NULL)printf("NULL\n");
   while(tmp!=NULL){
-    printf("(%d)",tmp->status);
+    printf("(%d,%d)",tmp->status,tmp->child_cnt);
     printf("%d->",tmp->pid);
     tmp=tmp->next;
   }
